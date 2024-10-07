d8cb15e5c33fd56e8cd368e7d5ea88df3b8e9295
Atris <vacekj@outlook.com>
2024-10-07 23:42:30 +0200

    feat: add tailwindˆ

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, theme, useColorMode } from '@chakra-ui/react';
 import { PenumbraUIProvider } from '@penumbra-zone/ui/PenumbraUIProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
+import './global.css';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
