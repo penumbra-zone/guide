671a2bd5cde1b432af7cfa1a08432785b555c9c5
Conor Schaefer <conorsch@users.noreply.github.com>
2025-02-03 12:27:28 -0800

    chore: update pd & cometbft versions (#52)
    The cometbft version bump is updated to include 0.37.15, to include
    patches to security issues. See details in [0].
    
    [0] https://github.com/cometbft/cometbft/blob/v0.37.15/CHANGELOG.md#v03715

--- penumbra_versions.js
+++ penumbra_versions.js
@@ -3,5 +3,5 @@
 // works in normal prose, but fails in backticked-codeblocks. There's some discussion
 // about how to do this better in https://github.com/orgs/mdx-js/discussions/2288.
 
-export const PENUMBRA_VERSION = 'v0.79.3';
-export const COMETBFT_VERSION = 'v0.37.9';
+export const PENUMBRA_VERSION = 'v1.0.1';
+export const COMETBFT_VERSION = 'v0.37.15';
